e6712477c02dbefcd7c42aa65e0b4ea90e4d46ea
MacOSX <macosx@MacOSXs-MacBook-Pro.local>
2019-12-14 01:32:41 -0500

    Fixing syntax according to PEP8

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import lxml.html
 import requests
 
+
 def get_request(url, num):
     return requests.get("{}?page={}".format(url, num))
+
 
 def get_companies(doc, pattern):
     companies = doc.xpath('//*[@class="startupCard__details__name"]/text()')
     return companies
 
+
 def set_companies(ret_companies, companies):
     return [ ret_companies.append(company.rstrip('\n')) for company in companies ]
+
 
 def get_all_companies():
     ret_companies = []
 
-    for num in range(1,20):
+    for num in range(1, 20):
         page = get_request("https://betalist.com/regions/australia", num)
         doc = lxml.html.fromstring(page.content)
 
@@ -25,5 +29,6 @@ def get_all_companies():
         set_companies(ret_companies, companies)
 
     return ret_companies
+
 
 print(get_all_companies())
--- test_companies.py
+++ test_companies.py
@@ -1,5 +1,6 @@
 import requests
 
+
 def test_get_request(requests_mock):
     requests_mock.get("https://betalist.com/regions/australia", text="Hello!")
 
